99739087fa29a132dd474ad15bf8587c13d6845b
PhhP <34850794+NorlaHan@users.noreply.github.com>
2018-04-29 20:49:52 +0900

    Initial commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Debug.Log ("Level load requested for : " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitLevel (string name) {
+		Debug.Log ("Level quit requested for : " + name);
+		Application.Quit();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
